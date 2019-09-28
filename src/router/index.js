e962dc13e5364f81aaa0e4e7571fd1cefc279854
valkmorgenstern <anastasiavalka@gmail.com>
2019-09-28 17:38:04 +0300

    main page added

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import MainPage from '../components/pages/MainPage'
+import FondsInfo from '../components/pages/FondsInfo'
+import Profile from '../components/pages/Profile'
+import News from '../components/pages/News'
+import Messages from '../components/pages/Messages'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'MainPage',
+      component: MainPage
+    },
+    {
+      path: '/fond/:id',
+      name: 'FondsInfo',
+      component: FondsInfo
+    },
+    {
+      path: '/user',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/news',
+      name: 'News',
+      component: News
+    },
+    {
+      path: '/messages',
+      name: 'Messages',
+      component: Messages
+    }
+
+  ]
+})
