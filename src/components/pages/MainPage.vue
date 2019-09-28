a1f2e934f2e61eea06a1b1739d7df14d78302800
valkmorgenstern <anastasiavalka@gmail.com>
2019-09-28 18:47:11 +0300

    MainPage added

--- src/components/pages/MainPage.vue
+++ src/components/pages/MainPage.vue
@@ -1,106 +1,158 @@
 <template>
   <component :is="isAuthorized?'LayoutAuthUs':'MainLayout'">
     <v-content id="page">
-      <v-flex d-flex xs12 sm12 md12>
+      <v-flex d-flex xs12 sm12 md12 lg12>
         <v-card id="info" style="margin: 1rem">
-            <v-container fill-height fluid>
-              <v-layout align-center justify-centert>
-                <v-flex xs12 align-center flexbox>
-                  <div style=" border: 3px solid #f1f1f1;">
-                  <div id="header"> Route in - это удобный сервис для построения маршрутов!</div>
-                  <div id="header2">Находите интересные места, объединяйте их в маршруты и делитесь ими с другими или
-                    исследуйте город по самым интересным маршрутам, созданными другими пользователями!
-                  </div>
-                  </div>
-                </v-flex>
-              </v-layout>
-            </v-container>
+          <v-container fill-height fluid>
+            <v-layout align-center justify-centert>
+              <v-flex align-center flexbox lg12>
+                <div>
+                  <div class="header" v-html="header1"></div>
+                  <div class="header2" v-html="header2"></div>
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-container>
         </v-card>
       </v-flex>
-      <br/>
-      <TripList :items="Route"></TripList>
+      <v-flex d-flex xs12 sm12 md12 lg12>
+        <v-card>
+          <v-container fluid>
+            <v-layout row align-center jusify-center wrap>
+              <v-card-title class="header">Что такое проект "Наставничество"?</v-card-title>
+            </v-layout>
+            <v-layout row wrap align-center justify-center>
+              <v-flex d-flex
+                      v-for="(item, index) in about"
+                      v-bind:key="index"
+                      xs6 lg3
+                      align-center justify-center>
+                <v-card height="350px" style="margin: 1rem; padding: 1rem">
+                  <v-card-title>
+                    <v-icon
+                      large
+                    >
+                      {{item.icon}}
+                    </v-icon>
+                    <v-divider light></v-divider>
+                    {{item.name}}
+                  </v-card-title>
+                  <v-divider light></v-divider>
+                  <div class="h3">{{item.description}}</div>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          <v-container fluid>
+            <v-layout row align-center jusify-center wrap>
+              <v-card-title class="header">Как это работает?</v-card-title>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
     </v-content>
   </component>
 </template>
 
-<script>
-  import Service from '../../services/Service'
-  import MainLayout from '@/components/layouts/MainLayout'
-  import LayoutAuthUs from '@/components/layouts/LayoutAuthUs'
-  import TripList from '@/components/components/TripList'
-  import {mapGetters} from 'vuex'
-
-  export default {
-    components: {
-      MainLayout,
-      TripList,
-      LayoutAuthUs
-    },
-    data() {
-      return {
-        message1: ' Route in - это удобный сервис для построения маршрутов. ',
-        message2: 'Находите интересные места, объединяйте их в маршруты и делитесь ' +
-          'ими с другими или исследуйте город по самым интересным маршрутам, созданными другими пользователями!',
-        Route: [],
-        selectedType: ''
-      }
-    },
-    computed: {
-      ...mapGetters(['isAuthorized'])
-    },
-    methods: {
-      async getRoute() {
-        const response = await Service.fetchRoute()
-        this.Route = response.data.routes
-        console.log(this.Route)
-      }
-    },
-    mounted() {
-      this.getRoute()
+<script>import Service from '../../services/Service'
+import MainLayout from '@/components/layouts/MainLayout'
+import LayoutAuthUs from '@/components/layouts/LayoutAuthUs'
+import {mapGetters} from 'vuex'
+export default {
+  components: {
+    MainLayout,
+    LayoutAuthUs
+  },
+  data() {
+    return {
+      header1: ' Проект «Наставничество» призван помочь\n' +
+        '                    показать ребенку жизнь за пределами детского дома ',
+      header2: 'Когда ребёнок живет в детском доме, мира взрослых как будто и не существует.\n' +
+        'Есть только воспитатели и дети, которым не хватает заботы и внимания.\n' +
+        'Поэтому только 10% из числа выпускников детского дома находят своё место во взрослой жизни,\n' +
+        'ведь откуда им о ней узнать?',
+      about: [
+        {
+          icon: 'school',
+          name: 'ПСИХОЛОГИ',
+          description: 'Проект сопровождают профессиональные психологи, которые несколько раз в месяц проводят с участниками групповые и индивидуальные супервизии, оценивают работу наставников, дают рекомендации, корректируют маршрутные листы детей'
+        },
+        {
+          icon: 'laptop_mac',
+          name: 'ЛИЧНЫЙ КАБИНЕТ',
+          description: 'Каждый Наставник имеет доступ к личному кабинету, где может составлять удобный график встреч с ребенком, получать быстрые советы от психолога, быть в курсе всех событий в рамках проекта'
+        },
+        {
+          icon: 'menu_book',
+          name: 'ЛИТЕРАТУРА',
+          description: 'На сайте хранится вся необходимая информация об индивидуальных особенностях детей, ссылки на полезные статьи и различная интересная информация о Наставничестве. Доступ в библиотеку открыт для зарегистрированных пользователей.'
+        },
+        {
+          icon: 'group',
+          name: 'СООБЩЕСТВО',
+          description: 'В рамках проекта Наставничество проходят тематические встречи наставников, детей и кураторов проекта. Участвуя в проекте, вы попадаете в сообщество неравнодушных людей тех, кому #невсеравно. Всегда можно обратиться за советом к тем, кто давно общается с детьми и получить необходимую информацию'
+        }
+      ],
+      howitworks: [
+        {
+        }
+      ]
     }
+  },
+  computed: {
+    ...mapGetters(['isAuthorized'])
+  },
+  methods: {
+    async getRoute() {
+      const response = await Service.fetchRoute()
+      this.Route = response.data.routes
+      console.log(this.Route)
+    }
+  },
+  mounted() {
+    this.getRoute()
   }
+}
 </script>
 
 <style scoped>
-  @import url('https://fonts.googleapis.com/css?family=Indie+Flower&display=swap');
-
-  #header {
+  @import url("https://fonts.googleapis.com/css?family=Comfortaa&display=swap");
+  .header {
     text-align: center;
-    font-family: 'Indie Flower', cursive;
+    font-family: 'Comfortaa', cursive;
     font-weight: bold;
-    background: rgba(205, 214, 219, 0.3);
+    /*background: rgba(205, 214, 219, 0.3);*/
     font-size: xx-large;
   }
-
-  #header2 {
+  .header2 {
     text-align: center;
-    font-family: 'Indie Flower', cursive;
+    font-family: 'Comfortaa', cursive;
     font-weight: bold;
     font-size: large;
-    background: rgba(205, 214, 219, 0.3);
+    /*background: rgba(205, 214, 219, 0.3);*/
   }
-
   #info {
     height: 400px;
-    background-image: url(../../components/pictures/Main_page_2.jpg);
+    background-image: linear-gradient(rgba(205, 214, 219, 0.3), #5a6370), url(http://www.nastavnik54.ru/assets/main_page/bg2-e55106e310b40363fc8a0d52b951c42dc696226954c248af8beb8ac5d82653f3.jpg);
     background-position: center center;
     background-repeat: no-repeat;
     background-size: cover;
+    background-blend-mode: darken
+  }
+  #tutorship {
+    height: 500px;
   }
   #map {
     margin: 15px;
     top: 50%;
   }
-
   #search {
     width: 50%;
     border: 1px solid;
     margin: 15px;
     background: white;
   }
-
   h3 {
-    font-family: 'Indie Flower', cursive;
+    font-family: 'Comfortaa', cursive;
   }
-
 </style>
