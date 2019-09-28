adc55abab5fc96ea1c99f0fe90fde2c7b121f6af
Veronika Plotnikova <Plotnikova99.99@gmail.com>
2019-09-28 17:27:50 +0300

    Initial commit

---
+++ src/components/components/MeetingsList.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="meetings-list">
+    <div v-for="(meeting,index) in meetings"
+         v-bind:key="index">
+      v-bind:class="['meeting', { active: currentMeetingId === meeting.id}]"
+      v-on:click="currentMeetingId=meeting.id">
+      <v-card hover>
+        <v-card-title>
+          <v-icon
+            v-if="meeting.status === 'checked'"
+            large
+            left
+            color="green"
+          >
+            check_circle_outline
+          </v-icon>
+          <v-icon
+            v-else
+            large
+            left
+            color="grey"
+          >
+            highlight_off
+          </v-icon>
+          <span class="title font-weight-normal">
+                {{ meeting.data }}
+              </span>
+        </v-card-title>
+        <v-card-text v-show="currentMeetingId === meeting.id">
+          <b>Статус: </b>{{ meeting.status }}
+        </v-card-text>
+        <!--<v-card-actions>-->
+        <!--<v-list-tile class="grow">-->
+        <!--<v-list-tile-content>-->
+        <!--<v-list-tile-title><b>Т: </b>{{ point.type }}</v-list-tile-title>-->
+        <!--</v-list-tile-content>-->
+        <!--</v-list-tile>-->
+        <!--</v-card-actions>-->
+      </v-card>
+    </div>
+  </div>
+</template>
+
+<script>export default {
+  name: 'MeetingsList',
+  props: {
+    meetings: [
+      {id: 0, status: 'watching', data: '11/04/1999'},
+      {id: 1, status: 'checked', data: '12/04/1999'},
+      {id: 2, status: 'checked', data: '13/04/1999'},
+      {id: 3, status: 'refused', data: '14/04/1999'},
+      {id: 4, status: 'checked', data: '15/04/1999'}
+    ]
+  },
+  data: function () {
+    return {
+      currentMeetingId: undefined
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+  #point-list {
+    margin: 0 15px;
+  }
+
+  #point-list .container.fluid {
+    padding: 0;
+  }
+
+</style>
---
+++ src/components/pages/Profile.vue
@@ -0,0 +1,127 @@
+<template>
+  <LayoutAuthUs>
+    <v-content>
+      <v-container fluid grid-list-md>
+        <v-layout row wrap>
+          <v-flex xs12 sm12 md6 lg4 align-center justify-center>
+            <v-card class="Avatar">
+              <div class="title mb-1"> {{ name }} {{ surname }}</div>
+              <div class="subheading"> {{ character }}</div>
+              <v-img
+                src="https://scontent-yyz1-1.cdninstagram.com/vp/4cb48e816c23553396618d5fbce4cd16/5E3A2038/t51.2885-15/e35/61371739_2297866336970576_3936695332403802332_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=108"
+                max-width="200" max-height="300"></v-img>
+              <v-list-tile-content>
+                <v-list-tile-title> Возраст: {{ age }}</v-list-tile-title>
+                <v-list-tile-title> Учреждение: {{ place }}</v-list-tile-title>
+                <v-list-tile-title> E-mail: {{ email }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-card>
+          </v-flex>
+          <v-flex d-flex xs12 lg8>
+            <v-layout row wrap>
+              <v-flex d-flex lg12 justify-center align-center>
+                <v-card>
+                  <v-card-text>Галерея</v-card-text>
+                  <v-carousel
+                    cycle
+                    height="400"
+                    hide-delimiter-background
+                    show-arrows-on-hover
+                  >
+                    <v-carousel-item
+                      v-for="(item, i) in albums"
+                      :key="i"
+                      :src="item.src"
+                      reverse-transition="fade-transition"
+                      transition="fade-transition"
+                    >
+                    </v-carousel-item>
+                  </v-carousel>
+                </v-card>
+              </v-flex>
+              <v-flex lg12>
+                <v-card>
+                  <v-card-title> Встречи</v-card-title>
+                  <meetings-list></meetings-list>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </LayoutAuthUs>
+</template>
+
+<script> import LayoutAuthUs from '../layouts/LayoutAuthUs'
+import MeetingsList from '../components/MeetingsList'
+
+export default {
+  components: {LayoutAuthUs, MeetingsList},
+  data() {
+    return {
+      name: 'Вероника',
+      surname: 'Плотникова',
+      patronymic: 'Сергеевна',
+      age: '20',
+      city: 'Санкт-Петербург',
+      email: 'Plotnikova99.99@gmail.com',
+      character: 'Frontend разработчик',
+      place: 'VK Hackathon',
+      foto: 'https://i.pinimg.com/736x/70/ae/ed/70aeed769408dede74aaf1818bd3bb32.jpg',
+      albums: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+        }
+      ]
+    }
+  }
+}
+// <!--import Service from '../../services/Service'-->
+// <!--export default {-->
+// <!--components: {-->
+// <!--LayoutAuthUs-->
+// <!--},-->
+// <!--data () {-->
+// <!--return {-->
+// <!--Saved: {-->
+// <!--Routes: []-->
+// <!--},-->
+// <!--User: {-->
+// <!--Routes: [],-->
+// <!--Info: {}-->
+// <!--}-->
+// <!--}-->
+// <!--},-->
+// <!--methods: {-->
+// <!--async insertData () {-->
+// <!--const response = await Service.ProfileInfo()-->
+// <!--this.User.Routes = response.data.author-->
+// <!--this.User.Info = response.data.profile-->
+// <!--this.Saved.Routes = response.data.likes-->
+// <!--},-->
+// <!--RoutePage (id){-->
+// <!--this.$router.push({ name: 'EditRoute', params: {id} })-->
+// <!--}-->
+// <!--},-->
+// <!--mounted () {-->
+// <!--this.insertData()-->
+// <!--}-->
+// <!--}-->
+</script>
+
+<style scoped>
+  .Avatar {
+    text-align: center;
+    font-weight: bold;
+  }
+</style>
